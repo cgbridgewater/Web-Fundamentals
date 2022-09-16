e482e9b393f70a2a5d4bde8d1fee5d1ae036f5f5
cgbridgewater <cgbridgewater@outlook.com>
2022-09-15 20:45:35 -0700

    first commit

---
+++ JS/Dojo Pizzaria/dojo_pizzeria.js
@@ -0,0 +1,8 @@
+var sandwich = {
+    bread:    "sourdough",
+    protein:  "london broil",
+    cheese:   "lacey swiss cheese",
+    toppings: ["romaine lettuce", "heirloom tomatoes", "horseradish sauce"]
+};
+    
+console.log(sandwich);
