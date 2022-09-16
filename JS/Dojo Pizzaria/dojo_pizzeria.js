a9bb5313d64f40bacc5edba8d9d348de19b57faa
cgbridgewater <cgbridgewater@outlook.com>
2022-09-15 22:57:01 -0700

    updating code 9_15_22

--- JS/Dojo Pizzaria/dojo_pizzeria.js
+++ JS/Dojo Pizzaria/dojo_pizzeria.js
@@ -1,8 +1,91 @@
-var sandwich = {
-    bread:    "sourdough",
-    protein:  "london broil",
-    cheese:   "lacey swiss cheese",
-    toppings: ["romaine lettuce", "heirloom tomatoes", "horseradish sauce"]
-};
+// get all items --> console log the parent variable
+
+// var sandwich = {
+//     bread:    "sourdough",
+//     protein:  "london broil",
+//     cheese:   "lacey swiss cheese",
+//     toppings: ["romaine lettuce", "heirloom tomatoes", "horseradish sauce"]
+// };
     
-console.log(sandwich);
+// console.log(sandwich);
+
+
+
+
+// var taco1 = {
+//     "tortilla": "soft corn tortilla",
+//     "protein":  "tinga chicken",
+//     "cheese":   "cotija cheese",
+//     "toppings": ["lettuce", "pico de gallo", "guacamole"],
+//     "tacoInfo": function() {
+//         console.log("Tortilla: " + this.tortilla);
+//         console.log("Protein:  " + this.protein);
+//         console.log("Cheese:   " + this.cheese);
+//         console.log("Toppings: " + this.toppings);
+//     }
+// }
+
+// taco1.tacoInfo();
+
+// console.log("Tortilla: " + taco1.tortilla);
+// console.log("Protein:  " + taco1.protein);
+// console.log("Cheese:   " + taco1.cheese);
+// console.log("Toppings: " + taco1.toppings);
+
+// var tortilla = "soft corn tortilla";
+// var protein = "tinga chicken";
+// var cheese = "cotija cheese";
+// var toppings = ["lettuce", "pico de gallo", "guacamole"];
+
+
+
+
+// function sandwichFactory(bread, protein, cheese, toppings) {
+//     var sandwich = {};
+//     sandwich.bread = bread;
+//     sandwich.protein = protein;
+//     sandwich.cheese = cheese;
+//     sandwich.toppings = toppings;
+//     return sandwich;
+// }
+    
+// var s1 = sandwichFactory("wheat", "turkey", "provolone", ["mustard", "fried onions", "arugula"]);
+// console.log(s1);
+
+
+
+
+
+
+
+// function pizzaOven(pizza1) {
+
+    var pizzeria = 
+    {
+    crustTpye: ["deep dish", "hand tosssed", "flat bread"],
+    sauceType: ["traditional", "marinara", "white sauce"],
+    cheeses: ["mozzarella","feta","pepper jack", "gouda"],
+    toppings: ["pepperoni", "sausage", "mushrooms", "olives", "onions", "canadian bacon", "pinapple", "bacon bits"],
+    }
+// return pizza 
+
+
+
+
+// pizza 1
+console.log("pizza 1- " + "crust: " + pizzeria.crustTpye[0] + ", sauce: " + pizzeria.sauceType[0] + ", cheese: " + pizzeria.cheeses[0] + ", toppings: " + pizzeria.toppings[0,1])    
+
+// pizza 2
+console.log("pizza 2- " + "crust: " + pizzeria.crustTpye[1] + ", sauce: " + pizzeria.sauceType[1] + ", cheese: " + pizzeria.cheeses[1] + ", toppings: " + pizzeria.toppings[2,3,4])    
+
+// pizza 3
+console.log("pizza 3- " + "crust: " + pizzeria.crustTpye[2] + ", sauce: " + pizzeria.sauceType[2] + ", cheese: " + pizzeria.cheeses[0] + ", toppings: " + pizzeria.toppings[5,6,7])    
+
+// pizza 4
+console.log("pizza 4- " + "crust: " + pizzeria.crustTpye[1] + ", sauce: " + pizzeria.sauceType[1] + ", cheese: " + pizzeria.cheeses[1]+ " + " + pizzeria.cheeses[2] + ", toppings: " + pizzeria.toppings[1])    
+
+
+
+
+
+
