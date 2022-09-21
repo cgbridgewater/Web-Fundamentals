3ce5f9065e2c033684712defbc12ef3ca4fdc828
cgbridgewater <cgbridgewater@outlook.com>
2022-09-20 22:05:34 -0700

    evening update 9/20/22

--- JS/4 function calculator/script.js
+++ JS/4 function calculator/script.js
@@ -1,1 +1,51 @@
-// do math
+console.log("page loaded... or is it??");
+
+var displayDiv = document.querySelector("#display");
+var num1 = "";
+var num2 = "";
+var operator = "";
+
+// number input
+function press(number) {
+  num1 += number;
+  displayDiv.innerText = num1;
+}
+
+// trigger operation keys
+function setOP(key) {
+  operator = key;
+  num2 = num1;
+  num1 = ""; 
+  // displayDiv.innerHTML = "0";
+}
+
+// trigger clear key
+function clr() {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  displayDiv.innerText = "0";
+}
+
+
+// do the maths
+function calculate() {
+var a = parseFloat (num2);
+var b = parseFloat (num1);
+var calc = 0;
+  if (operator == "+"){
+    calc = a + b
+  }
+  else if (operator == "-"){
+    calc = a - b
+  }
+  else if (operator == "*"){
+    calc = a * b
+  }
+  else if (operator == "/"){
+    calc = a / b
+  }
+  num1 = calc;
+  operator = "";
+  displayDiv.innerHTML = calc;
+}
