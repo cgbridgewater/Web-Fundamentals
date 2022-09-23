87a0137831e3f47a0319e32420c9cb004b4c6574
cgbridgewater <cgbridgewater@outlook.com>
2022-09-22 18:00:21 -0700

    update

--- JS/example file/index.js
+++ JS/example file/index.js
@@ -1,7 +1,6 @@
 // function displayName(){
 //     console.log('you are alive!');
 // }
-
 var nameTag = document.querySelector("#name-tag");
 function setName(element) {
     console.log(element.value);
